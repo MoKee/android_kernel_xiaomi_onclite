901051dc224a73b6a830ad7fa24a4ec88d7d5341
DennySPB <dennyspb@gmail.com>
2020-02-13 16:47:23 +0300

    sched: RIP WALT completely and aligin CFS with k4.19
    - added new CPU isolation code but keep old one for compatibility of some qcom drivers,
    - added and modified housekeeping functions for flags hodling
    - added some helpers from >k4.9
    - aligned core/topology fuctions with k4.19
    
    Code is misformatted now. Will prettify later
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -101,31 +101,6 @@ SCHED_FEAT(ENERGY_AWARE, false)
 #endif
 
 /*
- * Apply schedtune boost hold to tasks of all sched classes.
- * If enabled, schedtune will hold the boost applied to a CPU
- * for 50ms regardless of task activation - if the task is
- * still running 50ms later, the boost hold expires and schedtune
- * boost will expire immediately the task stops.
- * If disabled, this behaviour will only apply to tasks of the
- * RT class.
- */
-SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, false)
-
-/*
- * Inflate the effective utilization of SchedTune-boosted tasks, which
- * generally leads to usage of higher frequencies.
- * If disabled, boosts will only bias tasks to higher-capacity CPUs.
- */
-SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
-SCHED_FEAT(HISI_FILTER, true)
-
-/*
- * Use the Simplified Energy Model for EAS accounting only for
- * active costs of CPUs.
- */
-SCHED_FEAT(EAS_SIMPLIFIED_EM, true)
-
-/*
  * Fast pre-selection of CPU candidates for EAS.
  */
 SCHED_FEAT(FIND_BEST_TARGET, true)
